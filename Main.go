4713a743c82165152ab082d40daf6b3f2ed10281
Alexandre <alexandre@dafoe.me>
2022-05-02 20:40:11 -0400

    renaming

--- Main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "Concord/Authentication"
-
-func main() {
-	Authentication.Login()
-}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"Concord/Authentication"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%s", Authentication.Login())
+}
